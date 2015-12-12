77d3ff4f7e2879cdc0b37d3f436a54cd6ce8bb5d
Wilson Kurniawan <wkurniawan.92@gmail.com>
2015-12-13 07:26:23 +0800

    initial commit based on example provided by lecturer

---
+++ server/src/Runner.java
@@ -0,0 +1,14 @@
+package src;
+
+
+public class Runner
+{
+	static Server server;
+	final static int PORT = 9000;
+	
+	public static void main(String[] args){
+		server = new Server(PORT);
+	}
+	
+	
+}
